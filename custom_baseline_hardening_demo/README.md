5fdc830730f29b345eefbdce9479af0414d10e90
westcoast68 <62345972+westcoast68@users.noreply.github.com>
2021-05-13 16:06:58 -0700

    Delete custom_baseline_hardening_demo directory

--- custom_baseline_hardening_demo/README.md
+++
@@ -1,3 +0,0 @@
-## Custom Baseline Hardening Example ##
-
-- A simplistic approach to scanning, remediation, verification and rollback using Ansible Tower
--- custom_baseline_hardening_demo/roles/rhel_scan/files/1-1-0.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-if [[ $(grep -c -e "^server 10.10.10.10 iburst$" /etc/ntp.conf) -eq 1 && $(grep -c -e "^server 10.10.10.11 iburst$" /etc/ntp.conf) -eq 1 ]]; then
-    exit 0
-else
-    exit 1
-fi
